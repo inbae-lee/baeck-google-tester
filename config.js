2dca4a21a9a937a3e19d3a234b871ac8d03d13e6
inbae-lee <inbaelee@gmail.com>
2026-08-21 13:30:48 +0800

    Update Google OAuth Client ID in config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // client-side). Get it from Google Cloud Console > APIs & Services >
 // Credentials, and add this page's origin under "Authorized JavaScript
 // origins" for the client to work.
-const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "394344500550-rm3l29483m54c60fpuergmldevb2vb3k.apps.googleusercontent.com";
